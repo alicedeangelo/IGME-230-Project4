6aa7da4369d884e2a00f5cd3a96087617fd644c1
alicedeangelo <31452787+alicedeangelo@users.noreply.github.com>
2017-12-13 16:00:31 -0500

    Added comments and cleaned out some old code

--- weather/main.js
+++ weather/main.js
@@ -1,27 +1,33 @@
+//Changes the pane color to make it look stormy
 var changeStorm = anime({
     targets: 'div.pane',
     backgroundColor: '#95a4b2',
     autoplay: false,
 })
 
+//Changes the pane color to make it look sunny
 var changeSun = anime({
     targets: 'div.pane',
     backgroundColor: '#2fcbff',
     autoplay: true,
 })
 
+//Changes color of the sun when switching to stormy weather
 var turnOffSun = anime({
     targets: '#sun',
     backgroundColor: '#95a4b2',
     autoplay: false,
 })
 
+//Changes color of the sun when switching to sunny weather
 var turnOnSun = anime({
     targets: '#sun',
     backgroundColor: '#FFFF5F',
     autoplay: false,
 })
 
+//These four methods change the color of the divs that make up the cloud
+//Used when switching to stormy weather
 var turnOnCloudOne = anime({
     targets: '.cloud-square',
     backgroundColor: '#2F4F4F',
@@ -46,6 +52,8 @@ var turnOnCloudFour = anime({
     autoplay: false,
 })
 
+//These four methods change the color of the divs that make up the cloud
+//Used when switching to sunny weather
 var turnOffCloudOne = anime({
     targets: '.cloud-square',
     backgroundColor: '#ffffff',
@@ -70,6 +78,7 @@ var turnOffCloudFour = anime({
     autoplay: false,
 })
 
+//Calls methods when stormy button is pressed
 document.querySelector('.storm-button .restart').onclick = function() {
     turnOffSun.restart();
     turnOnCloudOne.restart();
@@ -79,6 +88,7 @@ document.querySelector('.storm-button .restart').onclick = function() {
     changeStorm.restart();
 },
 
+//Calls methods when sunny button is pressed
 document.querySelector('.sun-button .restart').onclick = function() {
     turnOnSun.restart();
     turnOffCloudOne.restart();
@@ -87,6 +97,3 @@ document.querySelector('.sun-button .restart').onclick = function() {
     turnOffCloudFour.restart();
     changeSun.restart();
 }
-
-//document.querySelector('.storm-button .restart').onclick = changeStorm.restart;
-//document.querySelector('.sun-button .restart').onclick = changeSun.restart;
